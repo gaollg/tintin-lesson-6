0017575288fe0367e17b5f757c1104a9ebb19019
Gaollg <gaollg@qq.com>
2023-04-20 21:09:56 +0800

    修复 hash 路由

--- .umirc.ts
+++ .umirc.ts
@@ -5,11 +5,13 @@ export default defineConfig({
     { path: '/', component: 'index' },
     { path: '/docs', component: 'docs' },
   ],
-
+  history: {
+    type: 'hash',
+  },
   npmClient: 'pnpm',
   tailwindcss: {},
   plugins: ['@umijs/plugins/dist/tailwindcss'],
-  base: '/tintin-lesson-6/dist/',
+  // base: '/tintin-lesson-6/dist/',
   publicPath: '/tintin-lesson-6/dist/',
   hash: true,
 });
