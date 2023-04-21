785104580b217013ddada7745528ea43ddb11a6b
XiangLi <xiangli.red@gmail.com>
2023-04-21 16:46:31 +0800

    增加登录信息

--- src/components/Web3Login.tsx
+++ src/components/Web3Login.tsx
@@ -1,14 +1,32 @@
-import { Button } from 'antd';
+import { Button, Divider, Card } from 'antd';
 import { useEffect, useState } from 'react';
 import Web3 from 'web3';
 let web3 = new Web3(window.ethereum);
 
+type TLoginInfo = {
+  chainId: number;
+  blockNumber: number;
+  blockTimestamp: string | number;
+  balance: string;
+};
+
 export default function Web3Login(props: { onLogin: () => void }, onLogout: () => void) {
   const [accounts, setAccounts] = useState<string[]>([]);
+  const [loginInfo, setLoginInfo] = useState<TLoginInfo>();
 
   let freshAccount = async () => {
     let accounts = await web3.eth.getAccounts();
     setAccounts(accounts);
+
+    let blockNumber = await web3.eth.getBlockNumber();
+    let block = await web3.eth.getBlock(blockNumber);
+    let loginInfo: TLoginInfo = {
+      chainId: await web3.eth.getChainId(),
+      blockNumber: blockNumber,
+      blockTimestamp: block.timestamp,
+      balance: await web3.eth.getBalance(accounts[0]),
+    };
+    setLoginInfo(loginInfo);
   };
   useEffect(() => {
     web3.eth.getAccounts().then((accounts) => {
@@ -16,19 +34,32 @@ export default function Web3Login(props: { onLogin: () => void }, onLogout: () =
     });
   }, []);
 
-  if (accounts.length > 0) {
+  if (loginInfo) {
     return (
-      <div>
-        <div>当前登录账号：{JSON.stringify(accounts)}</div>
-        <Button
-          onClick={async () => {
-            await window.ethereum.enable();
-            freshAccount();
-          }}
-        >
-          切换账号
-        </Button>
-      </div>
+      <Card title="登录信息" extra={<a href="#">More</a>} style={{ width: 500 }}>
+        <div className="max-w-[500px]">
+          <div className="flex flex-row  mt-2">
+            <div className="flex-1">登录账号</div>
+            <div className="flex-1 text-gray-400">{accounts[0]}</div>
+          </div>
+          <div className="flex flex-row  mt-2">
+            <div className="flex-1">chainId</div>
+            <div className="flex-1 text-gray-400">{loginInfo?.chainId}</div>
+          </div>
+          <div className="flex flex-row  mt-2">
+            <div className="flex-1">BlockNumber</div>
+            <div className="flex-1 text-gray-400">{loginInfo?.blockNumber}</div>
+          </div>
+          <div className="flex flex-row  mt-2">
+            <div className="flex-1">blockTimestamp</div>
+            <div className="flex-1 text-gray-400">{loginInfo?.blockTimestamp}</div>
+          </div>
+          <div className="flex flex-row  mt-2">
+            <div className="flex-1">余额</div>
+            <div className="flex-1 text-gray-400">{web3.utils.fromWei(loginInfo.balance)}</div>
+          </div>
+        </div>
+      </Card>
     );
   }
 
