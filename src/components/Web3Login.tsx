db877f4a43f5bfee47c0968c945aa350f6f248c2
XiangLi <xiangli.red@gmail.com>
2023-04-21 16:25:18 +0800

    增加账号登录

--- src/components/Web3Login.tsx
+++ src/components/Web3Login.tsx
@@ -1,19 +1,44 @@
 import { Button } from 'antd';
+import { useEffect, useState } from 'react';
 import Web3 from 'web3';
 let web3 = new Web3(window.ethereum);
 
-export default function Web3Login(props: {}) {
-  if (!window.ethereum) {
-    return <div>未安装钱包</div>;
+export default function Web3Login(props: { onLogin: () => void }, onLogout: () => void) {
+  const [accounts, setAccounts] = useState<string[]>([]);
+
+  let freshAccount = async () => {
+    let accounts = await web3.eth.getAccounts();
+    setAccounts(accounts);
+  };
+  useEffect(() => {
+    web3.eth.getAccounts().then((accounts) => {
+      freshAccount();
+    });
+  }, []);
+
+  if (accounts.length > 0) {
+    return (
+      <div>
+        <div>当前登录账号：{JSON.stringify(accounts)}</div>
+        <Button
+          onClick={async () => {
+            await window.ethereum.enable();
+            freshAccount();
+          }}
+        >
+          切换账号
+        </Button>
+      </div>
+    );
   }
+
   return (
     <div>
       <Button
         type="primary"
         onClick={async () => {
           await window.ethereum.enable();
-          let accounts = await web3.eth.getAccounts();
-          console.log(accounts);
+          freshAccount();
         }}
       >
         登录
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,8 +1,11 @@
-import yayJpg from '../assets/yay.jpg';
 import Web3 from 'web3';
 import Web3Login from '@/components/Web3Login';
 
 export default function HomePage() {
+  if (!window.ethereum) {
+    return <div>未安装钱包, 请安装钱包后重试</div>;
+  }
+
   return (
     <div>
       <Web3Login />
