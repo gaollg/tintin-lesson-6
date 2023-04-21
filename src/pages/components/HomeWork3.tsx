1049b277c582f30e06f6b2b71cdaa9fd4d18075c
Gaollg <gaollg@qq.com>
2023-04-21 19:53:11 +0800

    增加查询页面

--- src/pages/components/HomeWork3.tsx
+++ src/pages/components/HomeWork3.tsx
@@ -1,5 +1,5 @@
 import Web3Helper from '@/helper/Web3Heper';
-import { Button, Divider, Card } from 'antd';
+import { Button, Divider, Card, Space, InputNumber, Input } from 'antd';
 import { useEffect, useState } from 'react';
 let web3 = Web3Helper.instance();
 
@@ -17,6 +17,7 @@ export default function HomeWork3(props: {}) {
     tokenSymbol: string;
     totalSupply: string;
   }>();
+  let [queryAddress, setQueryAddress] = useState<string>('0x5Ee9F4Fe59aDd6b33bbe41fDaa26BC43af51681c');
   let fresh = async () => {
     let tokenSymbol = await contractLesson6ERC20.methods.symbol().call();
     let totalSupply = await contractLesson6ERC20.methods.totalSupply().call();
@@ -44,6 +45,22 @@ export default function HomeWork3(props: {}) {
           <div className="flex-1">总供应</div>
           <div className="flex-1 text-gray-400">{info?.totalSupply}</div>
         </div>
+        <Space.Compact style={{ width: '100%' }}>
+          <Input
+            value={queryAddress}
+            onChange={(e) => {
+              setQueryAddress(e.target.value);
+            }}
+          />
+          <Button
+            type="primary"
+            onClick={async () => {
+              alert('结果');
+            }}
+          >
+            查询
+          </Button>
+        </Space.Compact>
       </div>
     </Card>
   );
